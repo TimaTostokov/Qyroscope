64a1794cdc5047f9c240238742b3fad963d195ae
Tolik <t1005828@gmail.com>
2024-08-29 21:23:10 +0300

    Simple Qyroscope

---
+++ app/src/main/java/com/cleanlotos/qyroscope/MainActivity.java
@@ -0,0 +1,91 @@
+package com.cleanlotos.qyroscope;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager manager;
+
+    private Sensor sensor;
+
+    private ImageView imageView;
+
+    private TextView textView;
+
+    private SensorEventListener sensorEventListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView = findViewById(R.id.tv_textView);
+        imageView = findViewById(R.id.imageView);
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (manager != null) sensor = manager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        sensorEventListener = new SensorEventListener() {
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+
+                float[] rotationMatrix = new float[16];
+                SensorManager.getRotationMatrixFromVector(
+                        rotationMatrix, event.values);
+
+                float[] remappedRotationMatrix = new float[16];
+                SensorManager.remapCoordinateSystem(rotationMatrix,
+                        SensorManager.AXIS_Z,
+                        SensorManager.AXIS_Y,
+                        remappedRotationMatrix);
+
+                float[] orientations = new float[3];
+                SensorManager.getOrientation(remappedRotationMatrix, orientations);
+                for (int i = 0; i < 3; i++) {
+                    orientations[i] = (float) (Math.toDegrees(orientations[i]));
+                }
+
+                textView.setText(String.valueOf((int) orientations[2]));
+                imageView.setRotation(-orientations[2]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        manager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.unregisterListener(sensorEventListener);
+    }
+
+}
